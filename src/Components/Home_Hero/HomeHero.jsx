407215c81dbc837d1254c19196a563b7cc99c49e
mulbahoplanojames <mulbahjamesoplano@gmail.com>
2024-07-02 14:19:14 +0200

    Working on the hero section of the home page

--- src/Components/Home_Hero/HomeHero.jsx
+++ src/Components/Home_Hero/HomeHero.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const HomeHero = () => {
   return (
     <>
-      <div className="grid md:grid-cols-2 grid-cols-1 place-items-center  md:py-16 py-8  gap-14">
+      <div className="grid md:grid-cols-2 grid-cols-1 place-items-center  md:py-16 py-8  md:gap-6 gap-14">
         <div className="">
           <h1 className="md:text-5xl text-4xl uppercase font-black md:pr-72 pr-10 pb-6">
             Welcome <br /> we are here to help you
@@ -21,13 +21,13 @@ const HomeHero = () => {
           </Link>
         </div>
         <div
-          className=" bg-green-300 md:w-[60%] md:h-[24rem] w-[70%] h-[17rem] rounded-full relative"
+          className=" bg-green-300 md:w-[60%] md:h-[24rem] w-[70%] h-[17rem] rounded-full relative overflow-visible"
           id="item1"
         >
-          <div className="w-[6rem] h-[6rem] bg-red-300 rounded-full absolute -top-1 right-3"></div>
-          <div className="w-[6rem] h-[6rem] bg-yellow-300 rounded-full absolute bottom-[4rem] right-0"></div>
-          <div className="w-[6rem] h-[6rem] bg-orange-300 rounded-full absolute top-7 left-0"></div>
-          <div className="w-[6rem] h-[6rem] bg-purple-300 rounded-full absolute bottom-14 left-0"></div>
+          <div className="md:w-[6rem] md:h-[6rem] w-[5rem] h-[5rem] bg-red-300 rounded-full absolute top-1 right-5  z-20"></div>
+          <div className="md:w-[6rem] md:h-[6rem] w-[5rem] h-[5rem] bg-yellow-300 rounded-full absolute md:bottom-[4rem] bottom-6 md:right-0 -right-6"></div>
+          <div className="md:w-[6rem] md:h-[6rem] w-[5rem] h-[5rem] bg-orange-300 rounded-full absolute top-7 left-0"></div>
+          <div className="md:w-[6rem] md:h-[6rem] w-[5rem] h-[5rem] bg-purple-300 rounded-full absolute md:bottom-14 -bottom-2 md:left-0 left-4"></div>
         </div>
       </div>
     </>
