9057793070a2f2aa81c4bf7facc14366794a4a1d
mulbahoplanojames <mulbahjamesoplano@gmail.com>
2024-07-02 22:45:22 +0200

    Working on the hero section of the home page

---
+++ src/Components/Team/Team.jsx
@@ -0,0 +1,13 @@
+const Team = () => {
+  return (
+    <>
+      <div className="">
+        <h1 className="">ALSULK Leardership</h1>
+
+        <div className=""></div>
+      </div>
+    </>
+  );
+};
+
+export default Team;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import HomeHero from "../../Components/Home_Hero/HomeHero";
+import Team from "../../Components/Team/Team";
 
 const Home = () => {
   return (
     <div>
       <HomeHero />
+      <Team />
     </div>
   );
 };
