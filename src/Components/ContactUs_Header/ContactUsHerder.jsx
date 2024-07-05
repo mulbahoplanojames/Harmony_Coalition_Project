57a91df54dd15dae1a54a8f4ab7a3ec8684fd425
mulbahoplanojames <mulbahjamesoplano@gmail.com>
2024-07-05 13:15:41 +0200

    Working on the Contact Us page

---
+++ src/Components/ContactUs_Header/ContactUsHerder.jsx
@@ -0,0 +1,18 @@
+const ContactHeader = () => {
+  return (
+    <>
+      <div className="md:h-[50vh] h-[40vh] text-center lg:px-56 px-4 text-white bg-yellow_accent_3 flex justify-center items-center flex-col relative rounded-md mb-16 mt-6">
+        <h1 className="md:text-6xl text-4xl pb-4 uppercase font-bold">
+          Contact Us
+        </h1>
+        <p className="text-lg z-10">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus
+          neque iste officia. Reprehenderit culpa neque ut nisi, voluptatum
+          ratione sed!
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default ContactHeader;
