1367dde512a1ff69976071c49ac90d39ccf760a3
mulbahoplanojames <mulbahjamesoplano@gmail.com>
2025-02-11 23:07:39 +0200

    updated the VisitHero Component by changing the design

--- src/Pages/Visit_Pages/Visit_About/VisitAboutHero.jsx
+++ src/Pages/Visit_Pages/Visit_About/VisitAboutHero.jsx
@@ -3,45 +3,9 @@ import { Link } from "react-router-dom";
 
 import { motion } from "framer-motion";
 
-import { AuroraBackground } from "../../../Components/ui/aurora-background";
-
 const VisitAboutHero = () => {
   return (
     <>
-      {/* <AuroraBackground>
-        <div className="grid grid-cols-1 gap-10 px-5 py-16 md:grid-cols-2 place-items-center md:py-24 md:gap-3 md:px-6 lg:px-10 ">
-          <motion.div
-            animate={{ opacity: 0, x: -60 }}
-            whileInView={{ opacity: 1, x: 0 }}
-            transition={{ duration: 0.5, delay: 0.3 }}
-            className="pb-4 h-fit"
-          >
-            <h1 className="pb-6 pr-10 text-3xl font-black uppercase md:text-4xl md:pr-40 dark:text-white text-slate-950">
-              Association of liberian Students at kigali independent university
-            </h1>
-            <p className="pb-10 text-lg font-semibold dark:text-white text-slate-950">
-              The leadership of the Association of the Liberian Students at
-              Kigali Independent University welcomes you to the official
-              platform of the Association. We are here to render any assistance
-              you may require
-            </p>
-            <Link
-              to="/sign-up"
-              className="shadow-[10px 10px 10px -1px rgba(10,99,169,0.16), -10px,-10px,10px -1px rgba(255,255,255,0.70)] py-3 px-6 bg-primary_main text-lg text-white border-none rounded-full hover:bg-opacity-70"
-            >
-              Join Us
-            </Link>
-          </motion.div>
-
-          <motion.div
-            className=" md:w-[80%] md:h-[24rem] w-full h-[17rem]  relative overflow-visible border- border-primary_main "
-            id="item1"
-            initial={{ opacity: 0, x: 60 }}
-            whileInView={{ opacity: 1, x: 0 }}
-            transition={{ duration: 0.4, delay: 0.3 }}
-          ></motion.div>
-        </div>
-      </AuroraBackground> */}
       <section
         id="home"
         className="relative z-10 overflow-hidden  pb-8 pt-[70px]  md:pb-[100px] md:pt-[100px] xl:pb-[120px] xl:pt-[150px] 2xl:pb-[200px] 2xl:pt-[210px]"
@@ -59,7 +23,7 @@ const VisitAboutHero = () => {
                   transition={{ duration: 0.5, delay: 0.3 }}
                   className="pb-4 h-fit"
                 >
-                  <h1 className="pb-6 pr-10 text-3xl font-black uppercase md:text-4xl md:pr-30 dark:text-white text-slate-950">
+                  <h1 className="pb-6 text-3xl font-black uppercase md:text-4xl md:pr-30 dark:text-white text-slate-950">
                     Association of liberian Students at kigali independent
                     university
                   </h1>
